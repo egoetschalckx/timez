64b4ef59fe01dbcecfd6aebe16ca91f6ec4bd31e
Eric Goetschalckx <egoetschalckx@users.noreply.github.com>
2023-08-14 15:08:48 -0400

    add yargs and chalk for cli and colors

--- src/js/timez.js
+++ src/js/timez.js
@@ -1,14 +1,27 @@
-var dayjs = require('dayjs')
-var utc = require('dayjs/plugin/utc')
-var timezone = require('dayjs/plugin/timezone')
-var advancedFormat = require('dayjs/plugin/advancedFormat')
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
+import advancedFormat from 'dayjs/plugin/advancedFormat.js'
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+import chalk from 'chalk'
+
+const argv = yargs(hideBin(process.argv))
+    .default('tz', 'US/Eastern')
+    .default('ts', '2023-08-09T21:51:38.625+05:00')
+    .argv
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.extend(advancedFormat)
 
-const timestamp = '2023-07-14T20:22:46.697Z'
-const tz = "US/Central"
+const ts = argv.ts
+const tz = argv.tz
 
-let ts = dayjs(timestamp).tz(tz)
-console.log(ts.format('ddd, MMM DD YYYY hh:mm:ss.SSS A z (zzz)'))
+const convertedTs = dayjs(ts).tz(tz)
+const outputDay = convertedTs.format('ddd')
+const outputMDY = convertedTs.format('MMM DD YYYY')
+const outputHMS = convertedTs.format('hh:mm:ss.SSS A')
+const outputTz = convertedTs.format('z (zzz)')
+
+console.log(chalk.blue(outputDay) + ', ' + chalk.magenta(outputMDY) + ' ' + chalk.red(outputHMS) + ' ' + chalk.yellow(outputTz))
